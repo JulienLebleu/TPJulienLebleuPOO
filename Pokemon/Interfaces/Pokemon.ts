f5357aea4b5206e935ee190bd2bd62f2950dbda9
Julien LEBLEU <julien.18.12@live.fr>
2022-04-06 11:11:05 +0200

    crea arbo

---
+++ Pokemon/Interfaces/Pokemon.ts
@@ -0,0 +1,18 @@
+export default interface Pokemon{
+    /* attaquer(cible: Pokemon): void {
+        switch (cible.type) {
+        case cible.type = 'Eau': {
+            this.calculerDegatsContreEau()
+        break;
+        }
+        case cible.type = 'Feu': {
+            this.calculerDegatsContreFeu()
+        break;
+        }
+        case cible.type = 'Plante': {
+            this.calculerDegatsContrePlante()
+        break;
+        }
+        }
+        } */
+}
